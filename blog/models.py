21d081f09a4f88da8c949753b62c0fc71e0d1e1d
sergio <sergey.rybakov@mail.ru>
2024-07-26 21:28:58 +0300

    from 26/07/24

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200, verbose_name="Title")
     text = models.TextField(verbose_name="Post text")
     created_at = models.DateTimeField(default=timezone.now, verbose_name="Created at", editable=False)
-
+    image = models.ImageField(upload_to='posts/', null=True, verbose_name='Image')
     class Meta:
         # table name
         verbose_name = "Post"
--- myblog/urls.py
+++ myblog/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from myblog import settings
 from blog.views import root
 
 
@@ -24,5 +26,7 @@ urlpatterns = [
     path('', root),
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
-
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
